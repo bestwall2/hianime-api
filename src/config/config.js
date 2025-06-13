0748009dadb046932fe523aef172ca15653e9566
bestwall2 <85714557+bestwall2@users.noreply.github.com>
2025-06-13 13:42:00 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  baseurl: 'https://hianimez.to',
+  aniplayUrl: 'https://aniplay.lol/',
+
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0',
+    'Accept-Encoding': 'gzip, deflate',
+  },
+};
+export default config;
---
+++ src/controllers/homepage.controller.js
@@ -0,0 +1,15 @@
+import { axiosInstance } from '../services/axiosInstance';
+import { validationError } from '../utils/errors';
+import { extractHomepage } from '../extractor/extractHomepage';
+
+const homepageController = async (c) => {
+  const result = await axiosInstance('/home');
+
+  if (!result.success) {
+    throw new validationError(result.message);
+  }
+  const response = extractHomepage(result.data);
+  return response;
+};
+
+export default homepageController;
